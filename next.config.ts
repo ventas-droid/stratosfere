4ae6dfefede0ad5d7d083a3028e846e9fe6a2cef
Stratosfere Admin <ventas@bernabeurealty.com>
2026-03-20 16:42:50 +0100

    next

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
+      // 🔥 EL NUEVO SOPORTE DE FOTOS (CLOUDFLARE R2) QUE MONTAMOS AYER 🔥
+      { protocol: "https", hostname: "pub-91a7b06ba4714b50b36fb9d8df951419.r2.dev" },
     ],
     qualities: [60, 75], // ✅ añade 60 (y deja 75)
   },
